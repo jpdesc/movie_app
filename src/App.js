0f48401233fade5336b698db333e933ccf94ac3f
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-30 14:10:07 -0800

    Complete basic search function.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=c387c552';
 
-const movie1 = {
-    Title: 'Italian Spiderman',
-    Year: '2007',
-    imdbID: 'tt2705436',
-    Type: 'movie',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BYjFhN2RjZT…TkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg'
-}
-
 
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -37,8 +30,8 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => { } }
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value) }
                 />
                 <img
                     src={SearchIcon}
