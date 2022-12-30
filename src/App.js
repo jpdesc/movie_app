63db381d0d8546f76a8cacc668a14d592c1e38f1
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-29 17:32:39 -0800

    create searchMovies async function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// c387c552
+
+const API_URL = 'http://www.omdbapi.com?apikey=c387c552'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
     return (
         <h1>App</h1>
     );
